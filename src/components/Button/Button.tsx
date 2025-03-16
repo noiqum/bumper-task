763478fb4b1dd5f7c6ab613fd992ef2dd7db21c0
noiqum <onurcoskun99@gmail.com>
2025-03-16 12:33:55 +0300

    style: button component type added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,3 @@
-import { Span } from 'next/dist/trace';
 import React from 'react';
 
 type ButtonType = "green-base" | "green-long-rounded" | "dark-base";
@@ -13,11 +12,14 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ label, onClick, type, disabled, icon }) => {
+    const baseClass = "flex text-sm lg:text-lg lg:leading-6 leading-5 border border-solid cursor-pointer hover:shadow-md hover:scale-105 ";
+    const sharedGreenClass = "bg-primary-green text-primary-black border-primary-black hover:text-white";
+
     const typeClass: { [key in ButtonType]: string } = {
-        "green-base": "flex bg-primary-green text-primary-black text-sm lg:text-lg lg:leading-6 leading-5 rounded-sm px-3 py-1 leading-5 border border-primary-black border-solid cursor-pointer hover:text-white hover:shadow-md hover:scale-105 ",
-        "green-long-rounded": "bg-primary-green text-white rounded-full",
-        "dark-base": "flex items-center border-white   bg-primary-black text-white text-sm rounded-sm px-3 py-1 leading-5 border  border-solid cursor-pointer  "
-    }
+        "green-base": `${baseClass} ${sharedGreenClass} rounded-sm px-3 py-1`,
+        "green-long-rounded": `${baseClass} ${sharedGreenClass} rounded-full px-4 py-3 whitespace-nowrap`,
+        "dark-base": `${baseClass} items-center bg-primary-black text-white rounded-sm px-3 py-1 border-white`
+    };
 
     return (
         <button onClick={onClick} disabled={disabled} className={typeClass[type]}>
